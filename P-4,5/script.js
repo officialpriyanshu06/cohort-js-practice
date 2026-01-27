7c25e1557992ce23f3876f31ef8510f27f584220
officialpriyanshu06 <officialpriyanshu06@gmail.com>
2026-01-27 19:37:17 +0530

    js P-4,5

---
+++ P-4,5/script.js
@@ -0,0 +1,19 @@
+// Qn4:- print the word 'yes' 5 Times 
+
+"use strict";
+
+for(let i = 1; i < 6; i++){
+    console.log("yes");
+}
+
+
+// Qn5:- print whether numbers from 1 to 10 are even or odd 
+
+for(let i = 1; i <= 10; i++){
+    if(i%2 === 0){
+        console.log(i, "even no.");
+    } else{
+        console.log(i, "odd no.");
+    }
+}
+
