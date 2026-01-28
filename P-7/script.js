2752d7e6bbaecea131bebfba95931791e1767ad2
officialpriyanshu06 <officialpriyanshu06@gmail.com>
2026-01-28 20:51:37 +0530

    error handled

--- P-7/script.js
+++ P-7/script.js
@@ -1,9 +1,25 @@
 // Ask user’s age and check if eligible to vote. If age >= 18 → “Eligible”, else → “Not eligible”
 
-let age = +prompt("what is your age ? ");
+"use strict";
 
-if(age >= 18){
-    console.log("you're eligible to vote");
-} else {
-    console.log("you're not eligible to vote");
+let age = prompt("what is your age ? ");
+
+if(age === null){
+    console.error("you cancelled it")
+}else {
+    if(age.trim() === ""){
+        console.log("please write something...")
+    } else{
+        age = Number(age)
+        if(isNaN(age)){
+            console.log("please enter a number...")
+        }else{
+            if(age >= 18){
+                console.log("you're eligible to vote");
+            } else {
+                console.log("you're not eligible to vote");
+            }
+        }
+    }
 }
+
