2bc5b200d32368f111cae4adf25ce142f2ccd5bc
officialpriyanshu06 <officialpriyanshu06@gmail.com>
2026-01-30 23:50:10 +0530

    js P-10

--- P-10/script.js
+++ P-10/script.js
@@ -4,10 +4,8 @@ let password = "gohanbhai";
 
 let pass = prompt("enter your password");
 
-while(pass !== password){
-    if(pass === password){
-        console.log("access granted!");
-    }else {
-        console.log("access denied!");
-    }
+if(pass === password){
+    console.log("access granted!");
+} else{
+    console.log("access not granted!");
 }
