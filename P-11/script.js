5dc551aa53f479e0a62353aad243af2558289fe7
officialpriyanshu06 <officialpriyanshu06@gmail.com>
2026-01-31 22:48:57 +0530

    allow only 3 attempts

---
+++ P-11/script.js
@@ -0,0 +1,18 @@
+// Allow only 3 attempts to enter correct password. If user gets it right early, stop. If not → “Account locked”
+
+let sahipass = "gohanbhai";
+let pass = prompt("enter the passwod");
+let count = 0;
+
+while(pass !== sahipass){
+    count++
+    if(count === 3){
+        console.log("account locked");
+        break;
+    }
+    pass = prompt("enter the passwod");
+}
+
+if(pass === sahipass){
+    console.log("access granted");
+}
