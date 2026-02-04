19bca0bb94e7ef0a3b586f530f72fd8fa3c29b89
officialpriyanshu06 <officialpriyanshu06@gmail.com>
2026-02-04 23:15:05 +0530

    js p-19, switch case based

---
+++ P-19/script.js
@@ -0,0 +1,29 @@
+// WAP that prints day according to a given number. use switch case.
+
+let day = +prompt("Enter the day: ");
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
